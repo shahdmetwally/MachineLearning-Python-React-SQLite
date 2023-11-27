0ceda3a2ac40fd52a7c77e5c76cda547856968d8
Sepehr <sepehrmo@student.chalmers.se>
2023-11-27 09:01:55 +0100

    #18 adds new templates
    + smaller updates to existing templates & adds packages to Pipfile

--- AiDentity/main/urls.py
+++ AiDentity/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("history/", views.history, name="history"),
+    path("about/", views.about, name="about"),
 ]
--- AiDentity/main/views.py
+++ AiDentity/main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+# Create your views here.
+
+
 def home(request):
     return render(request, "home.html")
 
-# Create your views here.
+
+def history(request):
+    return render(request, "history.html")
+
+
+def about(request):
+    return render(request, "about.html")
