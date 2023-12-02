cfe342a0ed782a5b32f998c5d497a31783751010
metwally <gusmetwsh@student.gu.se>
2023-12-02 18:34:57 +0100

    #14 gets evaluation metrics for  retrained model

--- SQLite/server/admin.py
+++ SQLite/server/admin.py
@@ -3,42 +3,37 @@ from pathlib import Path
 import SQLite.server.model as model
 import logging
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('admin')
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.post('/upload')
+origins = ["http://localhost:3000", " http://localhost:3000/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#Admin should upload a new batch of data and retrain the model with it
+#Then use the retrained model to get the evaluation metrics and GET them
+@app.post('/retrain')
 def upload_and_retrain(db_file: UploadFile):
     try:
         # Save the uploaded SQLite database file to a temporary location
         temp_db_path = Path("temp_db.db")
-        try:
-            temp_db_path.touch()
-            logger.info("Temp file created successfully")
-        except Exception as e:
-            logger.error(f"Error creating temp file: {e}")
 
         with db_file.file as source_file, temp_db_path.open("wb") as temp_db:
-            try:
-                temp_db.write(source_file.read())
-            except Exception as e:
-                logger.error(f"Error copying file: {e}")
+            temp_db.write(source_file.read())
 
         # Assuming you have a method in your model module to retrain the model based on a database
-        retrained_model = model.retrain(temp_db_path)
-
-        # Convert retrained_model to a JSON-serializable format
-        #retrained_model_serializable = jsonable_encoder(retrained_model)
+        retrained_model, accuracy, precision, recall, f1 = model.retrain(temp_db_path)
 
         # Clean up temporary files
         temp_db_path.unlink()
 
-        #return JSONResponse(content={"message": "Data uploaded and model retrained successfully", "retrained_model": retrained_model})
-        return JSONResponse(content={"message": "Data uploaded and model retrained successfully"})
+        return JSONResponse(content={"message": "Data uploaded and model retrained successfully", "accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1})
     except Exception as e:
         return {"error": str(e)}
-
-#Then use the retrained model to get the evaluation metrics and GET them
