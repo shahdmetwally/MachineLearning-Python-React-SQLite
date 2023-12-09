626a669a187bef5629890e739cecc066f1bb4a4f
pokkias <pokkias@chalmers.se>
2023-12-09 21:18:19 +0100

    #26 Change style of buttons and make images show up

--- React/aidentity/src/components/UserHistory.js
+++ React/aidentity/src/components/UserHistory.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Button } from '@mui/material';
+
+const PredictionItem = ({ prediction }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = `http://127.0.0.1:8000/get_image/${prediction.image}`;
+    img.onload = () => setImageLoaded(true);
+    img.onerror = (error) => console.error('Error loading image:', error);
+  }, [prediction.image]);
+
+  return (
+    <li>
+      <p>Score: {prediction.score}</p>
+      {imageLoaded && <img src={`http://127.0.0.1:8000/get_image/${prediction.image}`} alt="Prediction" style={{ maxWidth: '100%' }} />}
+      <p>Created At: {prediction.created_at}</p>
+    </li>
+  );
+};
+
+
 
 const UserHistory = () => {
   const [predictions, setPredictions] = useState([]);
-  
+
   const fetchPredictions = () => {
     axios.get('http://127.0.0.1:8000/predictions')
       .then(response => {
@@ -18,11 +40,11 @@ const UserHistory = () => {
   return (
     <div>
       <h2>User Prediction History</h2>
-      <button onClick={fetchPredictions}>Show History</button>
+      <Button
+        variant="contained" onClick={fetchPredictions} style={{ backgroundColor: '#1A353E', color: 'white' }}>Show History</Button>
       <ul>
         {predictions.map((prediction, index) => (
-          <li key={index}>{JSON.stringify(prediction)}</li>
-          // Render each prediction item as needed
+          <PredictionItem key={index} prediction={prediction} />
         ))}
       </ul>
     </div>
--- React/aidentity/src/components/UserPredict.js
+++ React/aidentity/src/components/UserPredict.js
@@ -1,23 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
+import { Button } from '@mui/material';
 
 const UserPredict = () => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
   const [prediction, setPrediction] = useState(null);
+  const fileInputRef = useRef(null);
 
   const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+    const file = event.target.files[0];
+    setSelectedFile(file);
+    const previewURL = URL.createObjectURL(file);
+    setImagePreview(previewURL);
+    // Reset prediction and user feedback
+    setPrediction(null);
   };
 
   const handlePredict = () => {
-      const formData = new FormData();
-      formData.append('image', selectedFile);
+    const formData = new FormData();
+    formData.append('image', selectedFile);
 
-      axios.post('http://127.0.0.1:8000/predict', formData, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-          }
-      })
+    axios.post('http://127.0.0.1:8000/predict', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
       .then(response => {
         console.log(response);
         setPrediction(response.data.score);
@@ -25,15 +33,26 @@ const UserPredict = () => {
       .catch(error => {
         console.error('Error predicting:', error);
       });
-    };
-    console.log('Prediction:', prediction);
+  };
+
+  const triggerFileInput = () => {
+    fileInputRef.current.click();
+  };
+
   return (
     <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handlePredict}>Predict</button>
-      {prediction && <p>Prediction Score: {prediction}</p>}
-    </div>
-  );
+      <h2>Make A Prediction</h2>
+      <label htmlFor="file-upload" style={{ display: 'block', marginBottom: '10px' }}>
+        <Button variant="contained"style={{ backgroundColor: '#1A353E', color: 'white' }} onClick={triggerFileInput}>Choose file
+        </Button>
+        <input type="file"id="file-upload"onChange={handleFileChange}style={{ display: 'none' }}ref={fileInputRef}/>
+      </label>
+      {imagePreview && <img src={imagePreview} alt="Selected File" style={{ maxWidth: '100%', marginTop: '10px' }} />}
+      <Button
+        variant="contained" onClick={handlePredict} style={{ backgroundColor: '#1A353E', color: 'white' }}>Predict</Button>
+      {prediction && (<div><p>Prediction Score: {prediction}</p></div>)}
+      </div>
+      );
 };
 
 export default UserPredict;
