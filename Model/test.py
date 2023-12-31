efc894e948a8a7bba6be3515f598b6124720652a
pokkias <pokkias@chalmers.se>
2023-12-31 23:47:34 +0100

    #61 Creates tests for the three pipeline versions

--- Model/test.py
+++ Model/test.py
@@ -1,5 +1,9 @@
 import unittest
-from model_pipeline import LoadDataset
+from sklearn.pipeline import Pipeline
+from keras.models import Sequential
+from model_pipeline import LoadDataset, Preprocess, TrainModel, VisualizeFeatureMaps, EvaluateModel
+from model_pipeline import PreprocessVGG16, TrainModelVGG16
+from model_pipeline import PreprocessEfficientNet, TrainModelEfficientNet
 
 class TestDataset(unittest.TestCase):
     def test_load_dataset(self):
@@ -10,6 +14,73 @@ class TestDataset(unittest.TestCase):
 
         # Assert the loaded data
         self.assertEqual(num_classes, 158)
+
+
+class TestPipelines(unittest.TestCase):
+    
+    def test_pipeline_v1(self):
+
+        pipeline_v1 = Pipeline([
+            ('load_dataset', LoadDataset(train_database_path='./Model/Datasets/lfw_augmented_dataset.db')),
+            ('preprocess', Preprocess()),
+            ('train_model', TrainModel()),
+            ('visualize_feature_maps', VisualizeFeatureMaps()),
+            ('evaluate_model', EvaluateModel())
+        ])
+
+        # Run pipeline_v1 with sample data
+        accuracy, precision, recall, f1, model = pipeline_v1.fit_transform(None)
+        num_of_layers = 11
+        # Add assertions based on the expected outcome of the pipeline
+        assert accuracy < 1 
+        assert precision < 1
+        assert recall < 1
+        assert f1 < 1
+        self.assertIsInstance(model, Sequential)
+        self.assertTrue(hasattr(model, 'layers') and len(model.layers) == num_of_layers) 
+
+    def test_pipeline_v2(self):
+
+        pipeline_v2 = Pipeline([
+            ('load_dataset', LoadDataset(train_database_path='./Model/Datasets/lfw_augmented_dataset.db')),
+            ('preprocess', PreprocessVGG16()),
+            ('train_model', TrainModelVGG16()),
+            ('evaluate_model', EvaluateModel())
+        ])
+
+        # Run pipeline_v2 with sample data
+        accuracy, precision, recall, f1, model = pipeline_v2.fit_transform(None)
+        num_of_layers = 4
+        print(len(model.layers))
+        # Add assertions based on the expected outcome of the pipeline
+        assert accuracy < 1 
+        assert precision < 1
+        assert recall < 1
+        assert f1 < 1
+        self.assertIsInstance(model, Sequential)
+        self.assertTrue(hasattr(model, 'layers') and len(model.layers) == num_of_layers) 
+
+    def test_pipeline_v3(self):
+
+        pipeline_v3 = Pipeline([
+            ('load_dataset', LoadDataset(train_database_path='./Model/Datasets/lfw_augmented_dataset.db')),
+            ('preprocess', PreprocessEfficientNet()),
+            ('train_model', TrainModelEfficientNet()),
+            ('evaluate_model', EvaluateModel())
+        ])
+
+        # Run pipeline_v3 with sample data
+        accuracy, precision, recall, f1, model = pipeline_v3.fit_transform(None)
+        num_of_layers = 5
+        print(len(model.layers))
+        # Add assertions based on the expected outcome of the pipeline
+        assert accuracy < 1 
+        assert precision < 1
+        assert recall < 1
+        assert f1 < 1
+        self.assertIsInstance(model, Sequential)
+        self.assertTrue(hasattr(model, 'layers') and len(model.layers) == num_of_layers) 
+
 
 if __name__ == '__main__':
     unittest.main()
